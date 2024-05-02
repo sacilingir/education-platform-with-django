f1fe4922e1ce701f3b6323ea0f6aee17bb928158
Seyit Ahmet Çilingir <seyitacilingir@gmail.com>
2024-05-02 10:42:57 +0300

    category and tag has been added

--- smartedu_con/courses/models.py
+++ smartedu_con/courses/models.py
@@ -1,22 +1,30 @@
 from django.db import models
 
+
 class Category(models.Model):
-   name = models.CharField(max_length=50,null=True)
-   slug = models.SlugField(max_length=50,unique=True,null=True)
+    name = models.CharField(max_length=50, null=True)
+    slug = models.SlugField(max_length=50, unique=True, null=True)
 
-   def __str__(self):
-       return self.name
-       
+    def __str__(self):
+        return self.name
 
+class Tag(models.Model):
+    name = models.CharField(max_length=50, null=True)
+    slug = models.SlugField(max_length=50, unique=True, null=True)
 
-class Course(models.Model): #tablonun ismi Course
-    name = models.CharField(max_length=200,unique=True)
-    category = models.ForeignKey(Category,max_length=200,null=True,on_delete=models.DO_NOTHING)
-    description = models.TextField(blank=True,null=True)
-    image = models.ImageField(upload_to="courses/%Y/%m/%d/",default="courses/sampleimg.png")
-    date = models.DateTimeField(auto_now = True)
+    def __str__(self):
+        return self.name
+    
+
+class Course(models.Model):
+    
+    name = models.CharField(max_length=200, unique=True)
+    category = models.ForeignKey(Category, null=True, on_delete=models.DO_NOTHING)
+    tags = models.ManyToManyField(Tag, blank=True, null=True)    
+    description = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to="courses/%Y/%m/%d/", default="courses/default_course_image.jpg")
+    date = models.DateTimeField(auto_now=True)
     available = models.BooleanField(default=True)
-    #bu instancelar tabloda bulunan sütunlar 
 
     def __str__(self):
         return self.name
--- smartedu_con/courses/views.py
+++ smartedu_con/courses/views.py
@@ -1,26 +1,60 @@
 from django.shortcuts import render
-from . models import Course
+from . models import Course,Category,Tag
 
+  
 
 
 # Create your views here.
 def course_list(request):
 
     courses = Course.objects.all().order_by('-date')
+    categories = Category.objects.all()
+    tags = Tag.objects.all()
 
     context = {
-        'courses':courses
+        'courses':courses,
+        'categories':categories,
+        'tags':tags
     }
 
     return render(request,'courses.html',context)
 
 def course_detail(request,category_slug,course_id):
     course = Course.objects.get(category__slug=category_slug,id=course_id)
+    tags = Tag.objects.all()
 
     context = {
-        'course':course
+        'course':course,
+        'tags': tags
+        
+        
     }
 
     return render(request,'course.html',context)
 
 
+def category_list(request,category_slug):
+    
+    courses = Course.objects.all().filter(category__slug = category_slug) #category nin altındaki slug ı göstermek için __ eklenir
+    categories = Category.objects.all()
+    tags = Tag.objects.all()
+
+    context = {
+        'courses':courses,
+        'categories':categories,
+        'tags':tags
+    }
+    return render(request,'courses.html',context)
+
+def tag_list(request,tag_slug):
+    courses = Course.objects.all().filter(tags__slug =tag_slug)
+    categories = Category.objects.all()
+    tags = Tag.objects.all()
+
+    context = {
+        'courses':courses,
+        'categories':categories,
+        'tags':tags
+    }
+
+    return render(request,'courses.html',context)

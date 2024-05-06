937caa2558d48a4e4d67355619f8ed671b6b3d98
Seyit Ahmet Çilingir <seyitacilingir@gmail.com>
2024-05-06 20:39:34 +0300

    courses page has been revised

--- smartedu_con/courses/views.py
+++ smartedu_con/courses/views.py
@@ -1,11 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from . models import Course,Category,Tag
 
-  
 
-
-# Create your views here.
-def course_list(request):
+"""def course_list(request):
 
     courses = Course.objects.all().order_by('-date')
     categories = Category.objects.all()
@@ -17,20 +14,32 @@ def course_list(request):
         'tags':tags
     }
 
-    return render(request,'courses.html',context)
+    return render(request,'courses.html',context)"""
 
-def course_detail(request,category_slug,course_id):
-    course = Course.objects.get(category__slug=category_slug,id=course_id)
-    tags = Tag.objects.all()
+def course_list(request,category_slug=None,tag_slug=None):
+    category_page = None
+    tag_page=None
+    categories=Category.objects.all()
+    tags=Tag.objects.all()
+
+    if category_slug != None:
+        category_page = get_object_or_404(Category,slug = category_slug)
+        courses = Course.objects.filter(available = True,category = category_page)
+    
+    elif tag_slug != None:
+        tag_page = get_object_or_404(Tag,slug=tag_slug)
+        courses = Course.objects.filter(available = True,tags=tag_page)
+
+    else:
+        courses=Course.objects.all().order_by('-date')
 
     context = {
-        'course':course,
-        'tags': tags
-        
-        
-    }
+        'courses' : courses,
+        'categories' : categories,
+        'tags' : tags      
+        }
+    return render(request,'courses.html',context)
 
-    return render(request,'course.html',context)
 
 
 def category_list(request,category_slug):
@@ -46,6 +55,7 @@ def category_list(request,category_slug):
     }
     return render(request,'courses.html',context)
 
+
 def tag_list(request,tag_slug):
     courses = Course.objects.all().filter(tags__slug =tag_slug)
     categories = Category.objects.all()
@@ -58,3 +68,18 @@ def tag_list(request,tag_slug):
     }
 
     return render(request,'courses.html',context)
+
+
+
+def course_detail(request,category_slug,course_id):
+    course = Course.objects.get(category__slug=category_slug,id=course_id)
+    tags = Tag.objects.all()
+
+    context = {
+        'course':course,
+        'tags': tags
+    }
+
+    return render(request,'course.html',context)
+
+

5ee1569dd32376bddff9dadb9bd99d343487c4c7
Seyit Ahmet Çilingir <seyitacilingir@gmail.com>
2024-05-16 18:41:30 +0300

    dashboard page has been updated and some features fixed

--- smartedu_con/courses/views.py
+++ smartedu_con/courses/views.py
@@ -8,6 +8,7 @@ def course_list(request,category_slug=None,tag_slug=None):
     tag_page=None
     categories=Category.objects.all()
     tags=Tag.objects.all()
+    current_user = request.user
 
     if category_slug != None:
         category_page = get_object_or_404(Category,slug = category_slug)
@@ -18,7 +19,14 @@ def course_list(request,category_slug=None,tag_slug=None):
         courses = Course.objects.filter(available = True,tags=tag_page)
 
     else:
-        courses=Course.objects.all().order_by('-date')
+        if current_user.is_authenticated:
+            enrolled_courses = current_user.courses_joined.all()
+            courses=Course.objects.all().order_by('-date')
+            for course in enrolled_courses: #burada kayıt olunan dışındaki kursları gösteriyoruz courses sayfasında
+                courses = courses.exclude(id = course.id) 
+        else:
+            courses=Course.objects.all().order_by('-date')
+
 
     context = {
         'courses' : courses,
@@ -31,10 +39,21 @@ def course_list(request,category_slug=None,tag_slug=None):
 def course_detail(request,category_slug,course_id):
     course = Course.objects.get(category__slug=category_slug,id=course_id)
     tags = Tag.objects.all()
+    categories=Category.objects.all()
+    course = Course.objects.get(category__slug = category_slug, id = course_id)
+    courses=Course.objects.all().order_by('-date')
+    current_user = request.user
+    if current_user.is_authenticated:
+        enrolled_courses = current_user.courses_joined.all()
+    else:
+        enrolled_courses = courses
 
     context = {
         'course':course,
-        'tags': tags
+        'tags': tags,
+        'enrolled_courses' : enrolled_courses,
+        'categories' : categories
+
     }
 
     return render(request,'course.html',context)
